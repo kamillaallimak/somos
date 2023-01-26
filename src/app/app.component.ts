c285a4a90ca957a12c60fd94fb2d9300251503d0
Kamilla <kamilla@alshadidi.se>
2023-01-26 03:39:08 +0100

    contact started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'somos';
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.scroll, true);
+  }
+
+  scroll = (): void => {
+    let scrollHeigth;
+
+    if (window.innerWidth < 350) {
+      scrollHeigth = 20;
+    } else {
+      /*
+    else if (window.innerWidth < 500 && window.innerWidth > 350) {
+      scrollHeigth = 250;
+    } else if (window.innerWidth < 700 && window.innerWidth > 500) {
+      scrollHeigth = 350;
+    } else if (window.innerWidth < 1000 && window.innerWidth > 700) {
+      scrollHeigth = 500;
+    } 
+    */
+      scrollHeigth = 10 /*MAGIN!!!*/;
+    }
+
+    if (window.scrollY >= scrollHeigth) {
+      document.body.style.setProperty('--navbar-scroll', 'var(--green)');
+      document.body.style.setProperty('--navbar-scroll-text', 'white');
+      document.body.style.setProperty('--navbar - scroll - position', '50px');
+    } else if (window.scrollY < scrollHeigth) {
+      document.body.style.setProperty('--navbar-scroll', 'transparent');
+      document.body.style.setProperty('--navbar-scroll-text', 'white');
+      document.body.style.setProperty('--navbar - scroll - position', '0');
+    }
+  };
 }
