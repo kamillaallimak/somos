49d403650e313fb9d9d42ee421b98037090cc541
Kamilla <kamilla@alshadidi.se>
2023-01-31 18:58:22 +0100

    more responsive

---
+++ src/app/popChildComponents/pop-monday1/pop-monday1.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-monday1',
+  templateUrl: './pop-monday1.component.html',
+  styleUrls: ['./pop-monday1.component.css'],
+})
+export class ChildOneComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
