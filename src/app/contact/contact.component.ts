ce948b3fdb941b27f8a072e19cc33821d4601743
Kamilla <kamilla@alshadidi.se>
2023-03-01 23:22:01 +0100

    footer updated to current year variable

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {}
+export class ContactComponent {
+  ngOninit() {}
+
+  anio: number = new Date().getFullYear();
+}
